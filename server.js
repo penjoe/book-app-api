d4c1840e4cbe83b9184e5172e11d3e0ec55be3cc
‘penjoe’ <pennockjoe@gmail.com>
2020-04-24 01:43:40 -0700

    added a function tat queries the api for a list of results, maps those results, and generates new objects through the ocnstructor function. render those results onto show.ejs in an ejs template

--- server.js
+++ server.js
@@ -19,17 +19,48 @@ app.use(express.urlencoded({extended: true}));
 // EJS setup
 app.set('view engine', 'ejs');
 
-//Functions to handle various routes to/from server
+// Constructor function to generate new book Object based on API search results
+function Book(element) {
+  this.title = element.volumeInfo.title ? element.volumeInfo.title : 'No Title Available';
+  this.author = element.volumeInfo.authors ? element.volumeInfo.authors[0] : 'No Author Available';
+  this. description = element.volumeInfo.description ? element.volumeInfo.description : 'No Description Available';
+  this.isbn = element.volumeInfo.industryIdentifiers ? element.volumeInfo.industryIdentifiers[0] : 'No ISBN Available';
+  this.bookshelf = element.bookshelf;
+  this.image_url = element.volumeInfo.imageLinks ? element.volumeInfo.imageLinks.thumbnail : `https://i.imgur.com/J5LVHEL.jpg`;
+}
 
+// Functions to handle various routes to/from server
+
+// Function to handle Google Books API search and results
+function handleBooks(req, res) {
+  const { filter, bookQuery} = req.body;
+  const url = `https://www.googleapis.com/books/v1/volumes?q=+${filter}:${bookQuery}`;
+
+  superagent.get(url)
+    .then( bookResponse => {
+      let books = bookResponse.body.items;
+      return books.map( element => new Book(element));
+    })
+    .then( bookResults => {
+      res.render('./searches/show', {bookResults})
+    })
+    .catch( error => {})
+  
+}
 
 // REST/CRUD http routes
+app.use(cors());
+app.post('/searches', handleBooks);
 app.get('/searches/new', (req, res) => {
   res.render('searches/new')
 });
 
-// test route
+// test routes
 app.get('/test', (req, res) => {
   res.render('pages/index')
+});
+app.get('/searches/show', (req, res) => {
+  res.render('searches/show')
 });
 
 // Start server and listen for requests
